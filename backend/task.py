7d93ae7b83d5b7cf0e3cd64ff6f6fc1c4743d0b0
nrliangxy <nrliangxy@foxmail.com>
2018-07-10 18:16:12 +0800

    update

--- backend/task.py
+++ backend/task.py
@@ -4,13 +4,16 @@ import uuid
 import time
 import subprocess
 import os
+from uuid import uuid1
 import json
 from psutil import STATUS_ZOMBIE
-
+from database import TaskLog
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 from apscheduler.jobstores.base import JobLookupError
 from enum import Enum, unique
+
+
 # from common.logger_handler import LogHandler
 
 # logger = LogHandler('spider_shceduler')
@@ -31,18 +34,19 @@ class BackgroundTaskManager:
     #: constant indicating a scheduler's paused state (started but not processing jobs)
     STATE_PAUSED = 2
     """
+    
     def __init__(self, database: pymongo.MongoClient):
         self._scheduler = BackgroundScheduler()
         self._coll = database["manager"]["background"]
-
+    
     def start(self):
         assert self._scheduler.state != 1
         self._coll.update_many({}, {"$set": {"status": TaskStatus.STOP.value}})
         self._scheduler.start()
-
+    
     def shutdown(self):
         self._scheduler.shutdown()
-
+    
     def start_interval_job(self, func, trigger, name, kwargs=None, replace_existing=True):
         assert self._scheduler.state == 1
         record = self._coll.find_one({"name": name})
@@ -59,7 +63,7 @@ class BackgroundTaskManager:
                                 kwargs=kwargs,
                                 name=name,
                                 replace_existing=replace_existing)
-
+    
     def stop_interval_job(self, name):
         record = self._coll.find_one({"name": name})
         if record:
@@ -68,7 +72,7 @@ class BackgroundTaskManager:
             except JobLookupError as e:
                 pass
             self._coll.update({"_id": record["_id"]}, {"$set": {"status": TaskStatus.STOP.value}})
-
+    
     def list_task(self, state=None) -> list:
         """获取当前所有的定时任务"""
         result = []
@@ -96,12 +100,16 @@ class SchedulerTaskManager:
     def __init__(self, database: pymongo.MongoClient):
         self._database = database['manager']['command']
         self._scheduled_task = {}
-
+    
     def start_command_process(self, command_id, command):
         command = ['nohup'] + command
         process = psutil.Popen(command)
+        r_insert = {"task_id": str(uuid1()), "task_name": command_id, "pid_num": process.pid, "task_file_path": command[2]}
+        obj = TaskLog(**r_insert)
+        obj.save()
         self._scheduled_task.setdefault(command_id, process)
-
+        return command_id, process
+    
     def kill_process_by_pid(self, pid):
         while True:
             try:
@@ -110,7 +118,7 @@ class SchedulerTaskManager:
                 continue
             else:
                 break
-
+    
     def kill_process_by_name(self, process_name):
         """杀指定名称的进程"""
         p = self._scheduled_task.pop(process_name)
@@ -121,7 +129,7 @@ class SchedulerTaskManager:
                 continue
             else:
                 break
-
+    
     @classmethod
     def find_procs_by_name(cls, name):
         ls = []
@@ -131,16 +139,16 @@ class SchedulerTaskManager:
                             p.info['cmdline'] and p.info['cmdline'][0] == name:
                 ls.append(p)
         return ls
-
+    
     @staticmethod
     def reap_children(children_pid: list, timeout=3):
         """Tries hard to terminate and ultimately kill all the children of this process."""
         msg = "failed"
-
+        
         def on_terminate(proc):
             msg = "process {} terminated with exit code {}".format(proc, proc.returncode)
             return msg
-
+        
         procs = psutil.Process().children()
         # send SIGTERM
         for p in procs:
@@ -162,33 +170,42 @@ class SchedulerTaskManager:
                         msg = "process {} survived SIGKILL; giving up" % p
                         print("process {} survived SIGKILL; giving up" % p)
         return msg
-
+    
     def refresh_process(self):
         zombies = []
-        print(self._scheduled_task)
+        # print(self._scheduled_task)
         for pn, process in self._scheduled_task.items():
-            print(process.status())
+            # print(process.status())
             if process.status() == STATUS_ZOMBIE:
                 zombies.append(pn)
                 self.kill_process_by_pid(process.pid)
         for command_id in zombies:
             self._scheduled_task.pop(command_id)
+        return self._scheduled_task
+
+    def show_live_process(self, db_obj):
+        live_process = self.refresh_process()
+        live_list = list(live_process.keys())
+        for obj in db_obj.objects:
+            if obj.task_name not in live_list:
+                obj.update(status='stop')
 
 
 if __name__ == '__main__':
     import pymongo
     from urllib.parse import quote_plus
-
-
+    
+    
     def create_default_client():
         uri = "mongodb://%s:%s@%s" % (
             quote_plus("root"), quote_plus("root360"), "192.168.44.101:27100")
         return pymongo.MongoClient(uri)
-
-
+    
+    
     stm = SchedulerTaskManager(create_default_client())
     stm.start_command_process("1", ["python3", "lala.py"])
     while True:
         stm.refresh_process()
         import time
+        
         time.sleep(10)
