bed5035006770fd419b0b3299e54f89f2255a23c
haohua.yao <aaa>
2018-07-12 13:21:04 +0800

    fix bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,3 @@
 /root/anaconda3/bin/pip install health360 -i http://192.168.3.115:4040/patsnapci/dev/+simple/  --trusted-host 192.168.3.115
 ps aux | grep "manager_supervisord" | grep -v grep| cut -c 9-15 | xargs kill -9
 /usr/bin/supervisord -c manager_supervisord.conf
-/usr/bin/supervisorctl restart webui
-/usr/bin/supervisorctl restart celery_task
