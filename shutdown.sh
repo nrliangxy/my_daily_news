aa560f32a5d96a405561680af65a5bcde83bd1da
haohua.yao <aaa>
2018-07-12 13:30:47 +0800

    add shutdown shell

---
+++ shutdown.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+ps aux | grep "manager_supervisord" | grep -v grep| cut -c 9-15 | xargs kill -9
+ps aux | grep "celery_task" | grep -v grep| cut -c 9-15 | xargs kill -9
+ps aux | grep "run_manager_web" | grep -v grep| cut -c 9-15 | xargs kill -9
