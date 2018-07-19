31675121ae5db52bbfdd0cf13481890afc71cd6b
haohua.yao <aaa>
2018-07-19 16:40:00 +0800

    增加开发/测试质量检测库的选项

--- run_manager_web.py
+++ run_manager_web.py
@@ -3,6 +3,7 @@ from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 
+
 def run_tornado_server(host, port):
     app = create_default_app()
     print(app.config)
@@ -11,8 +12,5 @@ def run_tornado_server(host, port):
     IOLoop.instance().start()
 
 
-
 if __name__ == '__main__':
     run_tornado_server(host="0.0.0.0", port=5657)
-
-    
