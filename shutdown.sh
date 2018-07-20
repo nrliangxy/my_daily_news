f514ffe327fb54467929ef0b70c4d9f0e9f3e581
haohua.yao <aaa>
2018-07-20 10:15:12 +0800

    add supervisord conf

--- shutdown.sh
+++ shutdown.sh
@@ -2,3 +2,4 @@
 ps aux | grep "manager_supervisord" | grep -v grep| cut -c 9-15 | xargs kill -9
 ps aux | grep "celery_task" | grep -v grep| cut -c 9-15 | xargs kill -9
 ps aux | grep "run_manager_web" | grep -v grep| cut -c 9-15 | xargs kill -9
+ps aux | grep "run_backend_manager" | grep -v grep| cut -c 9-15 | xargs kill -9
