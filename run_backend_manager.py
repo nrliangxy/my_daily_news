a70b7114c182a375250699ef23bbc0968f6d05ad
nrliangxy <nrliangxy@foxmail.com>
2018-07-20 11:54:42 +0800

    update

--- run_backend_manager.py
+++ run_backend_manager.py
@@ -3,12 +3,15 @@
 
 from backend.task import SchedulerTaskManager
 import redis
-from datetime import datetime, timedelta
 from database.models import TaskLog
 from database import create_default_client, init_mongonengin_connect
 from backend.notify import Notification
-robot_api = "https://oapi.dingtalk.com/robot/send?access_token=9219c3463ad5a349650da5ecbe73ba2ffa5487ff36ad6cd8223ca668f305f9ee"
 
+from common.logger_handler import LogHandler
+
+logger = LogHandler('backend_manager')
+
+robot_api = "https://oapi.dingtalk.com/robot/send?access_token=9219c3463ad5a349650da5ecbe73ba2ffa5487ff36ad6cd8223ca668f305f9ee"
 
 # 初始化mongo服务
 init_mongonengin_connect()
@@ -47,36 +50,40 @@ def check_db(name=None):
 
 temp = []
 def start_command():
-    while True:
-        r = [i['task_name'] for i in check_db()[0] if i['status'] == 'stop']
-        if r:
-            r_2 = []
-            for i in r:
-                if i not in set(temp):
-                    r_2.append(i)
-            if r_2:
-                n = Notification(robot_api)
-                temp.extend(r)
-                print(n.remind("%s have been hung up" % str(r_2)))
-                continue
-        if R.lrange('form_data2', 0, -1):
-            form_data = R.lpop('form_data2')
-            if form_data != 1:
-                form_data = eval(form_data)
-                command = form_data.get('command')
-                command_list = command.split()
-                name = form_data.get('name')
-                db_task_name = show_task_name()
-                if name in db_task_name.keys():
-                    obj = db_task_name[name]
-                    pid = obj.pid_num
-                    if stm_manager.check_pid(pid):
-                        stm_manager.kill_process_by_pid(pid)
-                    obj.delete()
-                    show_live_process()
-                    stm_manager.start_command_process(name, command_list)
-                else:
-                    stm_manager.start_command_process(name, command_list)
+    try:
+        while True:
+            r = [i['task_name'] for i in check_db()[0] if i['status'] == 'stop']
+            if r:
+                r_2 = []
+                for i in r:
+                    if i not in set(temp):
+                        r_2.append(i)
+                if r_2:
+                    n = Notification(robot_api)
+                    temp.extend(r)
+                    print(n.remind("%s have been hung up" % str(r_2)))
+                    continue
+            if R.lrange('form_data2', 0, -1):
+                form_data = R.lpop('form_data2')
+                if form_data != 1:
+                    form_data = eval(form_data)
+                    command = form_data.get('command')
+                    command_list = command.split()
+                    name = form_data.get('name')
+                    db_task_name = show_task_name()
+                    if name in db_task_name.keys():
+                        obj = db_task_name[name]
+                        pid = obj.pid_num
+                        if stm_manager.check_pid(pid):
+                            stm_manager.kill_process_by_pid(pid)
+                        obj.delete()
+                        show_live_process()
+                        stm_manager.start_command_process(name, command_list)
+                    else:
+                        stm_manager.start_command_process(name, command_list)
+    except Exception as e:
+        logger.info(str(e))
+        
 
         
         
