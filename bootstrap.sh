40c1522a500152833096d320d5a19fa16a948ede
nrliangxy <nrliangxy@foxmail.com>
2018-08-08 11:21:18 +0800

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-/root/anaconda3/bin/pip -r install requirements.txt
+/root/anaconda3/bin/pip install -r requirements.txt
 /root/anaconda3/bin/pip uninstall health360 -y
 /root/anaconda3/bin/pip install health360 -i http://192.168.3.115:4040/patsnapci/dev/+simple/  --trusted-host 192.168.3.115
 ps aux | grep "manager_supervisord" | grep -v grep| cut -c 9-15 | xargs kill -9
